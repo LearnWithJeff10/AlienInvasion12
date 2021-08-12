12a76c59bdd065170de3debb738f1f0f82f446a0
Jeff <jm@jeffz.com>
2021-08-12 04:22:40 -0400

    Detect game over

--- alien_invasion.py
+++ alien_invasion.py
@@ -38,10 +38,12 @@ class AlienInvasion:
             """Start the main loop for the game."""
             while True:
                 self._check_events()
-                self.ship.update()
-                self._update_bullets()
-                self._update_aliens()
-                self._update_screen()
+
+                if self.stats.game_active:
+                    self.ship.update()
+                    self._update_bullets()
+                    self._update_aliens()
+                    self._update_screen()
 
 
     def _check_events(self):
@@ -180,19 +182,23 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to a ship being hit by an alien"""
 
-        # Decrement ships left
-        self.stats.ships_left -= 1
+        if self.stats.ships_left > 0:
+            # Decrement ships left
+            self.stats.ships_left -= 1
 
-        # Get rid of any remaining aliens and bullets
-        self.aliens.empty()
-        self.bullets.empty()
-        
-        # Create a new fleet and center the ship
-        self._create_fleet()
-        self.ship.center_ship()
-        
-        # Pause
-        sleep(0.5)
+            # Get rid of any remaining aliens and bullets
+            self.aliens.empty()
+            self.bullets.empty()
+            
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
+            
+            # Pause
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
+
          
 if __name__ == '__main__':
     # Make a game instance, and run the game.
