30496ba3e3393def70c863980c4a9d5e8d53327b
Jeff <jm@jeffz.com>
2021-08-12 20:44:12 -0400

    Deactivate play button if game is active (it wasn't visible, but it could be clicked anyway). Hide mouse when game is active.

--- alien_invasion.py
+++ alien_invasion.py
@@ -84,7 +84,8 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play."""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
             # Reset the game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -96,6 +97,9 @@ class AlienInvasion:
             # Create a new fleet and center the ship.
             self._create_fleet()
             self.ship.center_ship()
+
+            # Hide the mouse cursor
+            pygame.mouse.set_visible(False)
 
     def _fire_bullet(self): 
         """Create a new bullet and add it to the bullets group"""
@@ -223,6 +227,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
          
 if __name__ == '__main__':
